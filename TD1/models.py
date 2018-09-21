052123128f48e039c680cf8924b919c7b4e0c389
Rémi Garde <remi.garde@free.fr>
2018-09-21 11:04:48 +0200

    Tutorial 1: payroll basics

--- TD1/models.py
+++ TD1/models.py
@@ -1,14 +1,29 @@
 import random
-
+from datetime import datetime
 
 class Employee:
     __id_counter = 100000
-    def __init__(self, name, surname):
+    wage_points = 0
+
+
+    def __init__(self, name, surname, date=datetime.today()):
         self.id = Employee.__id_counter
         Employee.__id_counter += 1 # TODO: take care of overflow after 999 999
         self.name = name
         self.surname = surname
         self.login = "{}{}{}".format(name[1],surname[1], self.id)
+        self.arrival = date
+        self.raise_history = [] # ordered list that keeps track of the different raises a person had
+
+    def payroll(self, date=datetime.today()):
+        print("""Payroll for {self.name} {self.surname}
+Date: {date}
+
+Wage points: {self.wage_points} - Wage: {wage}€
+Remaining leave: TODO
+Remaining RTT: TODO
+        """.format(date=date, self=self, wage=self.wage_points*5))
+
 
 class Hourly(Employee):
     def __init__(self, name, surname):
