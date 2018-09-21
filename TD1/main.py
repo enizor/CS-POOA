cb217f9d1ab2e41871fb6e0fa965a436c38e6a85
Rémi Garde <remi.garde@free.fr>
2018-09-21 10:16:28 +0200

    Tutorial 1: every employee type init, with phones

--- TD1/main.py
+++ TD1/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Employee:
     __id_counter = 100000
     def __init__(self, name, surname):
@@ -5,18 +8,34 @@ class Employee:
         Employee.__id_counter += 1 # TODO: take care of overflow after 999 999
         self.name = name
         self.surname = surname
+        self.login = "{}{}{}".format(name[1],surname[1], self.id)
 
 class Hourly(Employee):
     def __init__(self, name, surname):
+        self.wage_points = 290
         super().__init__(name, surname)
 
-    wage_points = 290
-
 class Executive(Employee):
-    pass
+    def __init__(self, name, surname):
+        self.wage_points = 600
+        super().__init__(name, surname)
 
 class Commercial(Employee):
-    pass
+    def __init__(self, name, surname, phone_number=random.randint(100_000_000, 999_999_999)):
+        self.phone = Phone(number)
+        self.wage_points = 600
+        super().__init__(name, surname)
+
 
 class Director(Employee):
-    pass
+    def __init__(self, name, surname, phone_number=random.randint(100_000_000, 999_999_999)):
+        self.phone = Phone(phone_number)
+        self.wage_points = 1_000
+        super().__init__(name, surname)
+
+
+class Phone:
+    def __init__(self, phone_number):
+        # TODO: format with spaces and hyphens
+        self.number = "{:10}".format(phone_number)
+        self.mac = "{:0x}".format(random.randint(0, 2**48))
